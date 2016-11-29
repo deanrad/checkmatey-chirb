a49f08279622b60daa5fbf0681d57e16fa2c4365
Dean Radcliffe <deanmisc@yahoo.com>
2016-11-29 10:42:56 -0600

    Demonstrates pub-sub to populate a client-side collection

---
+++ client/compatibility/demo-script.js
@@ -0,0 +1,43 @@
+
+window.assert = (val, msg) => {
+    if (!val)
+        throw new Error(msg + ":" + val)
+    else
+        console.log('OK: ' + msg)
+}
+
+const context = (name, impl) => {
+    Tracker.autorun(() => {
+        console.log('Context: ' + name)
+        impl.call(null)
+    })
+}
+
+window.testIt = () => {
+    try {
+        context('Subscriptions are ready', () => {
+            // We'll be re-run when we become ready by doing this
+            if (!gameSubscription.ready()) return
+
+            // A Game has been populated in the Games collection, (via the subscription)
+            assert(Games.find().count() > 0, 'Expected to find a game')
+
+            // The game has a piece
+            assert(Games.findOne().board.position.d2,
+                `Expected piece at d2 ${Games.findOne().board.position.d2}`)
+
+        })
+
+        // Stopping a subscription removes records (must demo manually)
+        context('Stopping a subscription removes records', () => {
+            // gameSubscription.stop()
+            // assert(Games.find().count() === 0, 'Expected to find no games after stopping')
+        })
+
+        console.log('YAY all tests pass (TODO throw exception if not)')
+    } catch (err) {
+        console.log('Tests Failed: ', err)
+    }
+}
+
+Meteor.startup(window.testIt)
